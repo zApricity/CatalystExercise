cf9c72ef87d7dea64f38553863b9def24d4d7c90
zApricity <pdamiani1996@gmail.com>
2018-01-07 01:22:02 +1100

    Version 1.3

--- ScriptTask1.php
+++ ScriptTask1.php
@@ -4,62 +4,91 @@ if(!$dbConn) {
     exit("Failed to connect to database " . mysqli_connect_error());
 }
 
-$file=fopen('C:\xampp\htdocs\CatalystPractical\users.csv', 'r');
+$shortopts  = "";
+$shortopts .= "u:";
+$shortopts .= "p:";
+$shortopts .= "h:";
 
-$header=fgetcsv($file);
+$longopts  = array(
+    "file:",
+    "create_table",
+    "dry_run",
+);
+$options = getopt($shortopts, $longopts);
+var_dump($options);
 
-$escapedHeader=[];
+switch($argv[1]){
+    case "email":
+     $value=preg_replace('/(?![[:alnum:]]|@|-|_|\.)./', '', $value);
+    break;
+    case "name":
+     $value=preg_replace('/(?|!|@|-|_|\.)./', '', $value);
+     $value=preg_replace('/\s*/m', '', $value);
+     $value=strtolower($value);
+     $value=ucfirst($value);
+    break;
+    case "surname":
+     $value=preg_replace('/\s*/m', '', $value);
+     $value=preg_replace('/(?|!|@|-|_|\.)./', '', $value);
+     $value=strtolower($value);
+     $value=ucfirst($value);
+    break;
+    default:
+     echo "Unknown column!";
+}
 
-foreach ($header as $key => $value) {
-    $lheader=strtolower($value);
-    $escapedItem=preg_replace('/[^a-z]/', '', $lheader);
-
-    array_push($escapedHeader, $escapedItem);
-
+function parseCSV($file) {
+    $file=fopen('C:\xampp\htdocs\CatalystPractical\users.csv', 'r');
     
+    $header=fgetcsv($file);
+    
+    $escapedHeader=[];
+    
+    foreach ($header as $key => $value) {
+        $lheader=strtolower($value);
+        $escapedItem=preg_replace('/[^a-z]/', '', $lheader);
+        
+        array_push($escapedHeader, $escapedItem);
+    }
+    while($columns=fgetcsv($file))
+    {
+        if($columns[0]=="")
+        {
+            continue;
+        }
+        
+        $data=array_combine($escapedHeader, $columns);
+        
+        foreach ($data as $key => &$value)
+        {
+            switch($key){
+                case "email":
+                $value=preg_replace('/(?![[:alnum:]]|@|-|_|\.)./', '', $value);
+                break;
+                case "name":
+                $value=preg_replace('/(?|!|@|-|_|\.)./', '', $value);
+                $value=preg_replace('/\s*/m', '', $value);
+                $value=strtolower($value);
+                $value=ucfirst($value);
+                break;
+                case "surname":
+                $value=preg_replace('/\s*/m', '', $value);
+                $value=preg_replace('/(?|!|@|-|_|\.)./', '', $value);
+                $value=strtolower($value);
+                $value=ucfirst($value);
+                break;
+                default:
+                echo "Unknown column!";
+            }
+        }
+        
+        $name=$data['name'];
+        $surname=$data['surname'];
+        $email=$data['email'];
+        
+        echo "$name, $surname, $email", "\n";
+    }
 }
 
-while($columns=fgetcsv($file))
-{
-
-   if($columns[0]=="")
-   {
-       continue;
-   }
-
-   $data=array_combine($escapedHeader, $columns);
-
-   foreach ($data as $key => &$value)
-   {
-       switch($key){
-           case "email":
-            $value=preg_replace('/(?![[:alnum:]]|@|-|_|\.)./', '', $value);
-           break;
-           case "name":
-            $value=trim($value, " ");
-            $value=preg_replace('/(?| |!|@|-|_|\.)./', '', $value);
-            $value=strtolower($value);
-            $value=ucfirst($value);
-           break;
-           case "surname":
-            $value=preg_replace('/(?| |!|@|-|_|\.)./', '', $value);
-            $value=strtolower($value);
-            $value=ucfirst($value);
-           break;
-           default:
-            echo "Unknown column!";
-       }
-       
-   }
-
-
-   $name=$data['name'];
-   $surname=$data['surname'];
-   $email=$data['email'];
-
-   echo "$name, $surname, $email ";
-
-
-}
 
 ?>
